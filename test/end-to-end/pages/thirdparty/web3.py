714b4e2cfc7a7be8b6d488b9dca9a20777c676a0
leonprou <leonprou@gmail.com>
2018-05-05 23:02:49 +0300

    adding funding an issue

---
+++ test/end-to-end/pages/thirdparty/web3.py
@@ -0,0 +1,36 @@
+from web3 import Web3, HTTPProvider
+# from web3.contract import ConciseContract
+# from ethereum.transactions import Transaction
+import rlp
+
+
+token_abi = '''
+[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_amount","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"creationBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_newController","type":"address"}],"name":"changeController","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_blockNumber","type":"uint256"}],"name":"balanceOfAt","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_cloneTokenName","type":"string"},{"name":"_cloneDecimalUnits","type":"uint8"},{"name":"_cloneTokenSymbol","type":"string"},{"name":"_snapshotBlock","type":"uint256"},{"name":"_transfersEnabled","type":"bool"}],"name":"createCloneToken","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"parentToken","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_owner","type":"address"},{"name":"_amount","type":"uint256"}],"name":"generateTokens","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_blockNumber","type":"uint256"}],"name":"totalSupplyAt","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"transfersEnabled","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"parentSnapShotBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_owner","type":"address"},{"name":"_amount","type":"uint256"}],"name":"destroyTokens","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_token","type":"address"}],"name":"claimTokens","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenFactory","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_transfersEnabled","type":"bool"}],"name":"enableTransfers","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"controller","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"_tokenFactory","type":"address"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_token","type":"address"},{"indexed":true,"name":"_controller","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"ClaimedTokens","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_cloneToken","type":"address"},{"indexed":false,"name":"_snapshotBlock","type":"uint256"}],"name":"NewCloneToken","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Approval","type":"event"}]
+'''
+
+def fund_issue(private_key, eth_account, eth_provider, to, ammount=1):
+    provider = HTTPProvider(eth_provider)
+    web3 = Web3(provider)
+    print('balance: ', web3.eth.getBalance(eth_account))
+    token_contract = web3.eth.contract(address='0xc55cF4B03948D7EBc8b9E8BAD92643703811d162', abi=token_abi)
+    print('balance SNT: ', token_contract.functions.balanceOf('0x6f00cE7253bFD3A5A1c307b5E13814BF3433C72f').call())
+
+    transaction = token_contract.functions.transfer(to, ammount).buildTransaction({
+        'from': eth_account,
+        'nonce': web3.eth.getTransactionCount(eth_account) + 1,
+        'gas': 300459,
+        'gasPrice': 5000000000,
+        'value': 0
+    })
+    print(transaction)
+    signed = web3.eth.account.signTransaction(transaction, private_key)
+    print(signed)
+    print('send:')
+    print(web3.eth.sendRawTransaction(signed.rawTransaction).hex())
+
+    # tx = Transaction(
+    #     gasprice=web3.eth.gasPrice,
+    #     to=to,
+    #     value=0,
+    #     data=b'',
+    # )
--- test/end-to-end/tests/test_contracts.py
+++ test/end-to-end/tests/test_contracts.py
@@ -1,45 +1,52 @@
 import pytest
 from unittest import TestCase
-from pages.openbounty.bounties import BountiesPage
-from pages.openbounty.activity import ActivityPage
+# from pages.openbounty.bounties import BountiesPage
+# from pages.openbounty.activity import ActivityPage
 from pages.thirdparty.github import get_contract_address
-from tests.basetestcase import BaseTestCase
+from pages.thirdparty.web3 import fund_issue
+
+# from tests.basetestcase import BaseTestCase
 from tests import test_data
-
-
-@pytest.mark.sanity
-class TestLogin(BaseTestCase):
-
-     def test_deploy_new_contract(self):
-
-        # Waiting for deployed contract; test_data.issue created here
-        self.github_org.create_new_bounty()
-        self.github_org.get_deployed_contract()
-
-        # Navigate and check top bounty in "Open bounties"
-        bounties_page = BountiesPage(self.driver_dev)
-        bounties_page.get_bounties_page()
-        titles = bounties_page.bounty_titles.find_elements()
-        assert titles[0].text == test_data.issue['title']
-
-     def test_new_claim(self):
-        self.github_dev.create_pr_git('test_branch_%s' % self.github_dev.time_now)
-        self.github_dev.open_pr_github('Fixes')
-
-        # check new claim in "Open bounties"
-        bounties_page = BountiesPage(self.driver_dev)
-        bounties_page.get_bounties_page()
-        bounties_page.check_bounty_claims_amount(test_data.issue['title'], '1 open claim')
-
-        # check new claim in "Activity"
-        activity_page = ActivityPage(self.driver_dev)
-        activity_page.get_activity_page()
-        activity_page.check_activity_is_presented('Submitted a claim for ', test_data.issue['title'])
+#
+# @pytest.mark.sanity
+# class TestLogin(BaseTestCase):
+#
+#      def test_deploy_new_contract(self):
+#
+#         # Waiting for deployed contract; test_data.issue created here
+#         self.github_org.create_new_bounty()
+#         self.github_org.get_deployed_contract()
+#
+#         # Navigate and check top bounty in "Open bounties"
+#         bounties_page = BountiesPage(self.driver_dev)
+#         bounties_page.get_bounties_page()
+#         titles = bounties_page.bounty_titles.find_elements()
+#         assert titles[0].text == test_data.issue['title']
+#
+#      def test_new_claim(self):
+#         self.github_dev.create_pr_git('test_branch_%s' % self.github_dev.time_now)
+#         self.github_dev.open_pr_github('Fixes')
+#
+#         # check new claim in "Open bounties"
+#         bounties_page = BountiesPage(self.driver_dev)
+#         bounties_page.get_bounties_page()
+#         bounties_page.check_bounty_claims_amount(test_data.issue['title'], '1 open claim')
+#
+#         # check new claim in "Activity"
+#         activity_page = ActivityPage(self.driver_dev)
+#         activity_page.get_activity_page()
+#         activity_page.check_activity_is_presented('Submitted a claim for ', test_data.issue['title'])
 
 
 @pytest.mark.sanity
 class TestFunding(TestCase):
     def test_implement_bounty_funding(self):
-        issue_link = 'https://github.com/NewOrGanizationSOB/RepoSOB1_2/issues/145'
-        address = get_contract_address(issue_link)
-        self.assertEqual(address, '0x5e2ae47063c55902b160738d6c4c217638df6da7')
+        # issue_link = 'https://github.com/NewOrGanizationSOB/RepoSOB1_2/issues/145'
+        # address = get_contract_address(issue_link)
+        # self.assertEqual(address, '0x5e2ae47063c55902b160738d6c4c217638df6da7')
+        private_key = test_data.config['Ethereum']['eth_private_key']
+        eth_account = test_data.config['Ethereum']['eth_account']
+        eth_provider = test_data.config['Ethereum']['eth_provider']
+        fund_issue(private_key, eth_account, eth_provider, to='0x5E2aE47063c55902b160738d6C4C217638df6Da7')
+
+        self.assertEqual(0, 1)
